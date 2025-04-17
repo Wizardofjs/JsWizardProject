f448adbe3e1f586e87c2a4d16d72ca08cc403a1d
Tom Larsson <tmlsn@hotmail.com>
2025-04-17 10:37:42 +0200

    mitt förslag, sparar fetch till sessionStorage

--- modules/posts.js
+++ modules/posts.js
@@ -1,43 +1,45 @@
-// Hämta post från session storage
 function getDataFromSessionStorage(key) {
   const data = sessionStorage.getItem(key);
   return data ? JSON.parse(data) : [];
 }
-//Funktion för att visa första användare och dess inlägg
+
 export function showPosts() {
   const posts = getDataFromSessionStorage('posts');
 
   if (posts.length > 0) {
     const postsContainer = document.querySelector('.post');
+
     postsContainer.innerHTML = '';
 
-    // Object för att hålla koll på vilken användare
-    const seenUsers = new Set();
+    // Count the number of posts per user
+    // Track post counts for each user
+    const postCounts = {};
 
-    // Filtrera 1 post per användare
-    const uniquePosts = posts.filter((post) => {
-      if (!seenUsers.has(post.userId)) {
-        seenUsers.add(post.userId);
-        return true;
+    posts.forEach((post) => {
+      if (!postCounts[post.userId]) {
+        postCounts[post.userId] = 0;
       }
-      return false;
-    });
 
-    // Visa varje användare och dess post.
-    // Här ska ett id skapas för kommentarer alt. i HTML sidan.
-    uniquePosts.forEach((post) => {
+      // Increment the count for the user
+      postCounts[post.userId] += 1;
+
       const article = document.createElement('article');
       article.classList.add('post');
 
       const header = document.createElement('header');
-      const h2 = document.createElement('h2');
 
-      /* Bara för att se vilken användare och vilken post, ta bort vid senare tillfälle */
-      h2.textContent = `User: ${post.userId} | Post ID: ${post.id}`;
+      /* Tillfälligt för att se vilken användare som har vilken post */
+      const h2 = document.createElement('h2');
+      /*  h2.textContent = `Användare: ${post.userId} > post: ${post.id}`; */
+      h2.textContent = ` ${post.userId} user | post:${
+        postCounts[post.userId]
+      }/10 | total:${post.id} `;
       header.appendChild(h2);
+
       article.appendChild(header);
 
       const pContent = document.createElement('p');
+      // Replace \n with <br> to preserve line breaks in the body
       pContent.innerHTML = post.body.replace(/\n/g, '<br>');
       article.appendChild(pContent);
 
