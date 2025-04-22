ade0f8f06a07d111bbd5412eea14e44b53486fc4
hannasahlberg <hannassahlberg@live.se>
2025-04-22 09:20:26 +0200

    kommentarer

--- modules/user.js
+++ modules/user.js
@@ -1,17 +1,20 @@
+//Importerar fetchUsers från fetch modulen
 import { fetchUsers } from './fetch.js';
-
+//Skapar en async funktion loadUsers()
 async function loadUsers(){
     try{
+        //Tillderar variabler
         const users = await fetchUsers;
         const userDiv = document.querySelector('user-div');
-
+        //Loopar igenom users och skapar element i html
         users.forEach(user => {
             const userElement = document.createElement('div');
             userElement.textContent = `${user.name}, ${user.username}, ${user.email}`
             userDiv.appendChild(userElement);
         });
+        //Fångar eventuella errors 
     }catch (error){
-        console.log('fel vid hämtning av användare' + error);
+        console.log('Fel vid hämtning av användare' + error);
     }
 }
 
