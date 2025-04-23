59820d36b19dc486a094a2bdc6a6534eb35fbe5e
Tom Larsson <tmlsn@hotmail.com>
2025-04-23 13:43:18 +0200

    la til i main.js

--- modules/main.js
+++ modules/main.js
@@ -1,11 +1,13 @@
 import { fetchAndStoreData } from './fetch.js';
 import { showPosts } from './posts.js';
 import { loadUsers } from './user.js';
+import { loadWand } from './ui.js';
 
 async function init() {
   await fetchAndStoreData();
   loadUsers();
   showPosts();
+  loadWand();
 }
 
 init();
