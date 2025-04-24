9d6472993ade123cad9cd27357df66b0f7c39160
hannasahlberg <hannassahlberg@live.se>
2025-04-24 10:24:36 +0200

    posts och profilbild

--- modules/user.js
+++ modules/user.js
@@ -1,15 +1,18 @@
 //Importerar fetchUsers från fetch modulen
 import { getDataFromSessionStorage } from './fetch.js';
 import { renderTodos } from './todos.js';
+import { showAllPostUser } from './posts.js';
+
 
 //Skapar en async funktion loadUsers()
 export function loadUsers(){
     try{
         //Tillderar variabler
+        const images = getDataFromSessionStorage('img');
         const users = getDataFromSessionStorage('users');
         const userDiv = document.querySelector('.others-div');
         //Loopar igenom users och skapar element i html
-        users.forEach(user => {
+        users.forEach((user, index) => {
             const userElement = document.createElement('button');
             const colors = ['green', 'gray'];
             const randomColor = colors[Math.floor(Math.random() * colors.length)];
@@ -18,12 +21,15 @@ export function loadUsers(){
             userElement.classList.add('other-user');
             userElement.textContent = `${user.name}`;
             userDiv.appendChild(userElement);
+
+            const userImage = images[index];
             //Skapar en eventlyssnare som anropar en funktion
             userElement.addEventListener('click', () => {
-                showUserDetails(user);
+                showUserDetails(user, userImage?.image);
                 renderTodos(user.id);
+                showAllPostUser(user.id);
             })
-
+            console.log(`Bild för ${user.name}:`, userImage?.image);
         });
         //Fångar eventuella errors 
     }catch (error){
@@ -32,8 +38,12 @@ export function loadUsers(){
 }
 
 //Funktion som anropas från eventlyssnaren och öppnar mer information om den klickade användaren
-function showUserDetails(user){
+function showUserDetails(user, img){
     const userDiv = document.querySelector('.user-ui');
-    userDiv.innerHTML = `<i class="fa-solid fa-user"></i><br> <h2>${user.name}</h2><br> <p>Username: ${user.username}<br>Email: ${user.email}</p>`
-
+    userDiv.innerHTML = 
+    `<i class="fa-solid fa-user"></i>
+    <br> <h2>${user.name}</h2><br> 
+    <img src="${img || 'default.jpg'}" alt="Profilbild" class="profile-pic"><br>
+    <p>Username: ${user.username}<br>
+    Email: ${user.email}</p>`;
 }
